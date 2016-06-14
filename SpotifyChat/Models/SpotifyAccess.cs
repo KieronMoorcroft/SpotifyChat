c1710579d4b6f9fbacfb69c72e2cca13d88490eb
Kieronm <kieron.j.moorcroft95@gmail.com>
2016-06-14 18:39:44 +0100

    Few changes on SpotifyAccess model

--- SpotifyChat/Models/SpotifyAccess.cs
+++ SpotifyChat/Models/SpotifyAccess.cs
@@ -13,6 +13,35 @@ namespace SpotifyChat.Models
 {
     public class SpotifyAccess
     {
-       
+        private SpotifyWebAPI _spotify;
+        private PrivateProfile _profile;
+
+        private List<FullTrack> GetSavedTracks()
+        {
+            Paging<SavedTrack> savedTracks = _spotify.GetSavedTracks();
+            List<FullTrack> list = savedTracks.Items.Select(track => track.Track).ToList();
+
+            while (savedTracks.Next != null)
+            {
+                savedTracks = _spotify.GetSavedTracks(20, savedTracks.Offset + savedTracks.Limit);
+                list.AddRange(savedTracks.Items.Select(track => track.Track));
+            }
+
+            return list;
+        }
+
+        private List<SimplePlaylist> GetPlaylists()
+        {
+            Paging<SimplePlaylist> playlists = _spotify.GetUserPlaylists(_profile.Id);
+            List<SimplePlaylist> list = playlists.Items.ToList();
+
+            while (playlists.Next != null)
+            {
+                playlists = _spotify.GetUserPlaylists(_profile.Id, 20, playlists.Offset + playlists.Limit);
+                list.AddRange(playlists.Items);
+            }
+
+            return list;
+        }
     }
 }
